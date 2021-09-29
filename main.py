8e56017213e27558f7f18c2881a419be0328b46d
GraduationBear <maxime.miquel2@gmail.com>
2021-09-29 21:04:55 +0200

    Appretissage de wx

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 import numpy as np
+import wx
 
-print(np.sqrt(9))
+class MyFrame(wx.Frame):
+    def __init__(self, parent, title):
+        wx.Frame.__init__(self, parent, title=title, size=(200,100))
+        self.control = wx.TextCtrl(self, style=wx.TE_MULTILINE)
+        self.Show(True)
+
+app = wx.App(False)
+frame = MyFrame(None, 'Small editor')
+app.MainLoop()
