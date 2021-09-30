30d2e336bc13a4f5bce68c3c473b2f6830503474
GraduationBear <maxime.miquel2@gmail.com>
2021-09-30 17:27:12 +0200

    Ajout des classes Banque et Joueur

---
+++ src/Banque.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class Banque {
+	
+	private ArrayList<Carte> jeu;
+	
+	public Banque() {
+		this.jeu = new ArrayList<>();
+	}
+	
+	public ArrayList<Carte> getJeu(){
+		return this.jeu;
+	}
+	
+	public void setJeu(Carte carte) throws Exception{
+		if (this.jeu.size()>2) {
+			throw new Exception();
+		}
+		this.jeu.add(carte);
+	}
+	
+	
+}
---
+++ src/Joueur.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+
+public class Joueur {
+	private ArrayList<Carte> jeu;
+	private int solde;
+	
+	public Joueur() {
+		this.jeu= new ArrayList<>();
+		this.solde=0;
+	}
+	
+	public void setSolde(int solde) {
+		this.solde=solde;
+	}
+	
+	public int getSolde() {
+		return this.solde;
+	}
+	
+	public void setJeu(Carte carte) throws Exception{
+		if (this.jeu.size()>2) {
+			throw new Exception();
+		}
+		this.jeu.add(carte);
+	}
+	
+	public ArrayList<Carte> getJeu(){
+		return this.jeu;
+	}
+}
